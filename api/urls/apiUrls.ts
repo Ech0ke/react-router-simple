c4e77ca3744210ca1d0b050b8d8f2125d9598974
Ech0ke <aidas.zygas@gmail.com>
2023-08-10 14:42:41 +0300

    Added api urls

---
+++ api/urls/apiUrls.ts
@@ -0,0 +1,5 @@
+const BASE_URL = "http://127.0.0.1:3000";
+
+export const GET_POSTS = `${BASE_URL}/posts`;
+export const GET_TODOS = `${BASE_URL}/todos`;
+export const GET_USERS = `${BASE_URL}/users`;
