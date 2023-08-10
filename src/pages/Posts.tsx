c63ec0aaf92898afe3e74d6025e5bf77fc18fb4d
Ech0ke <aidas.zygas@gmail.com>
2023-08-10 14:43:15 +0300

    Made posts,users,todos pages render dynamic data

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -1,107 +1,25 @@
-import React from "react";
+import { useLoaderData, Link } from "react-router-dom";
+import { PostType } from "../types/postType";
 
 function Posts() {
+  const posts = useLoaderData() as PostType[];
   return (
     <div className="container">
       <h1 className="page-title">Posts</h1>
       <div className="card-grid">
-        <div className="card">
-          <div className="card-header">
-            sunt aut facere repellat provident occaecati excepturi optio
-            reprehenderit
-          </div>
-          <div className="card-body">
-            <div className="card-preview-text">
-              quia et suscipit suscipit recusandae consequuntur expedita et cum
-              reprehenderit molestiae ut ut quas totam nostrum rerum est autem
-              sunt rem eveniet architecto
+        {posts.map((post) => (
+          <div className="card" key={post.id}>
+            <div className="card-header">{post.title}</div>
+            <div className="card-body">
+              <div className="card-preview-text">{post.body}</div>
+            </div>
+            <div className="card-footer">
+              <Link className="btn" to={post.id.toString()}>
+                View
+              </Link>
             </div>
           </div>
-          <div className="card-footer">
-            <a className="btn" href="post.html">
-              View
-            </a>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-header">qui est esse</div>
-          <div className="card-body">
-            <div className="card-preview-text">
-              est rerum tempore vitae sequi sint nihil reprehenderit dolor
-              beatae ea dolores neque fugiat blanditiis voluptate porro vel
-              nihil molestiae ut reiciendis qui aperiam non debitis possimus qui
-              neque nisi nulla
-            </div>
-          </div>
-          <div className="card-footer">
-            <a className="btn" href="post.html">
-              View
-            </a>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-header">
-            ea molestias quasi exercitationem repellat qui ipsa sit aut
-          </div>
-          <div className="card-body">
-            <div className="card-preview-text">
-              et iusto sed quo iure voluptatem occaecati omnis eligendi aut ad
-              voluptatem doloribus vel accusantium quis pariatur molestiae porro
-              eius odio et labore et velit aut
-            </div>
-          </div>
-          <div className="card-footer">
-            <a className="btn" href="post.html">
-              View
-            </a>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-header">eum et est occaecati</div>
-          <div className="card-body">
-            <div className="card-preview-text">
-              ullam et saepe reiciendis voluptatem adipisci sit amet autem
-              assumenda provident rerum culpa quis hic commodi nesciunt rem
-              tenetur doloremque ipsam iure quis sunt voluptatem rerum illo
-              velit
-            </div>
-          </div>
-          <div className="card-footer">
-            <a className="btn" href="post.html">
-              View
-            </a>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-header">nesciunt quas odio</div>
-          <div className="card-body">
-            <div className="card-preview-text">
-              repudiandae veniam quaerat sunt sed alias aut fugiat sit autem sed
-              est voluptatem omnis possimus esse voluptatibus quis est aut
-              tenetur dolor neque
-            </div>
-          </div>
-          <div className="card-footer">
-            <a className="btn" href="post.html">
-              View
-            </a>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-header">dolorem eum magni eos aperiam quia</div>
-          <div className="card-body">
-            <div className="card-preview-text">
-              ut aspernatur corporis harum nihil quis provident sequi mollitia
-              nobis aliquid molestiae perspiciatis et ea nemo ab reprehenderit
-              accusantium quas voluptate dolores velit et doloremque molestiae
-            </div>
-          </div>
-          <div className="card-footer">
-            <a className="btn" href="post.html">
-              View
-            </a>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,88 +1,42 @@
-import React from "react";
+import { useLoaderData, Link } from "react-router-dom";
+import { UserType } from "../types/userType";
 
 function Users() {
+  const users = useLoaderData() as UserType[];
   return (
     <div className="container">
       <h1 className="page-title">Users</h1>
       <div className="card-grid">
-        <div className="card">
-          <div className="card-header">Leanne Graham</div>
-          <div className="card-body">
-            <div>Romaguera-Crona</div>
-            <div>hildegard.org</div>
-            <div>Sincere@april.biz</div>
+        {users.map((user) => (
+          <div className="card" key={user.id}>
+            <div className="card-header">{user.name}</div>
+            <div className="card-body">
+              <div>
+                <span className="bold">Company name: </span>
+                {user.company.name}
+              </div>
+              <div>
+                <span className="bold">Website: </span>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={`https://${user.website}`}
+                >
+                  {user.website}
+                </a>
+              </div>
+              <div>
+                <span className="bold">Email: </span>
+                {user.email}
+              </div>
+            </div>
+            <div className="card-footer">
+              <Link className="btn" to={user.id.toString()}>
+                View
+              </Link>
+            </div>
           </div>
-          <div className="card-footer">
-            <a className="btn" href="user.html">
-              View
-            </a>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-header">Ervin Howell</div>
-          <div className="card-body">
-            <div>Deckow-Crist</div>
-            <div>anastasia.net</div>
-            <div>Shanna@melissa.tv</div>
-          </div>
-          <div className="card-footer">
-            <a className="btn" href="user.html">
-              View
-            </a>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-header">Clementine Bauch</div>
-          <div className="card-body">
-            <div>Romaguera-Jacobson</div>
-            <div>ramiro.info</div>
-            <div>Nathan@yesenia.net</div>
-          </div>
-          <div className="card-footer">
-            <a className="btn" href="user.html">
-              View
-            </a>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-header">Patricia Lebsack</div>
-          <div className="card-body">
-            <div>Robel-Corkery</div>
-            <div>kale.biz</div>
-            <div>Julianne.OConner@kory.org</div>
-          </div>
-          <div className="card-footer">
-            <a className="btn" href="user.html">
-              View
-            </a>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-header">Chelsey Dietrich</div>
-          <div className="card-body">
-            <div>Keebler LLC</div>
-            <div>demarco.info</div>
-            <div>Lucio_Hettinger@annie.ca</div>
-          </div>
-          <div className="card-footer">
-            <a className="btn" href="user.html">
-              View
-            </a>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-header">Mrs. Dennis Schulist</div>
-          <div className="card-body">
-            <div>Considine-Lockman</div>
-            <div>ola.org</div>
-            <div>Karley_Dach@jasper.info</div>
-          </div>
-          <div className="card-footer">
-            <a className="btn" href="user.html">
-              View
-            </a>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
