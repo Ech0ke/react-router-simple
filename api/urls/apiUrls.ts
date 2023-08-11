a3c64337ba46ba237ac70c808394ba9c2969729d
Ech0ke <aidas.zygas@gmail.com>
2023-08-11 10:18:57 +0300

    Added development url in .env.development

--- api/urls/apiUrls.ts
+++ api/urls/apiUrls.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "http://127.0.0.1:3000";
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const GET_POSTS = `${BASE_URL}/posts`;
 export const GET_TODOS = `${BASE_URL}/todos`;
